dafd6defd3006cf92507ef4c3d87b7269f39b392
Miru <itmir913@gmail.com>
2024-12-29 08:36:24 +0900

    해시 계산을 한 번만 하도록

--- configs/hash.py
+++ configs/hash.py
@@ -2,6 +2,9 @@ import hashlib
 import os
 import sys
 
+# 캐시를 위한 딕셔너리 생성
+hash_cache = {}
+
 
 def sha_256_hash():
     # PyInstaller로 패키징한 후 실행되는 경우 sys._MEIPASS가 설정됨
@@ -10,12 +13,18 @@ def sha_256_hash():
     else:
         script_path = __file__
 
+    # 캐시에 해시가 이미 저장되어 있는지 확인
+    if script_path in hash_cache:
+        return hash_cache[script_path]
+
     # 파일을 열어서 내용을 읽어들임
     with open(script_path, 'rb') as file:
         file_content = file.read()
 
     # SHA-256 해시 계산
     sha256_hash = hashlib.sha256(file_content).hexdigest()
-    print(sha256_hash)
+
+    # 해시를 캐시에 저장
+    hash_cache[script_path] = sha256_hash
 
     return sha256_hash
