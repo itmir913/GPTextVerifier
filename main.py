7f4a0e2275394c07374135afdb7ed061fa9e4c51
Miru <itmir913@gmail.com>
2024-12-29 02:07:26 +0900

    Add Menubar in MainUI

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from tkinter import Frame, Label, Button, StringVar
+import tkinter as tk
+import webbrowser
+from tkinter import Frame, Label, Button, StringVar, messagebox
 
 from tkinterdnd2 import TkinterDnD, DND_FILES
 
+from configs.hash import sha_256_hash
 from text_differ.gui_text_differ import text_differ
 from text_verifier.gui_text_verifier import TextVerifier
 
 # 기본 폰트 설정
 default_font = ("맑은 고딕", 25)
 xlsx_file_path = ""
+
+# 제작자 및 버전 정보
+AUTHOR = "운양고등학교 이종환T"
+VERSION = "2024.12.29"
 
 
 def create_main_window():
@@ -25,6 +32,15 @@ def create_main_window():
     configure_root_layout(root)
     create_top_frame(root, file_path_var)
     create_bottom_frame(root, file_path_var)
+
+    # 메뉴바 생성
+    menubar = tk.Menu(root)
+    about_menu = tk.Menu(menubar, tearoff=0)
+    about_menu.add_command(label="프로그램 정보", command=on_info_click)
+    about_menu.add_command(label="GitHub 바로가기", command=on_github_click)
+    about_menu.add_command(label="Hash 생성", command=lambda: on_hash_click(root))
+    menubar.add_cascade(label="About", menu=about_menu)
+    root.config(menu=menubar)
 
     root.mainloop()
 
@@ -123,6 +139,38 @@ def create_button(frame, text, column, bg_color, command):
         command=command
     )
     button.grid(row=0, column=column, sticky="nsew", padx=10, pady=10)
+
+
+def on_info_click():
+    info_title = "프로그램 정보"
+    info_message = (
+        f"제작자: {AUTHOR}\n"
+        f"버전: {VERSION}\n"
+        "\n"
+        "GPTextVerifier는 OpenAI의 ChatGPT와 연동하여 한국어 맞춤법 및 문맥을 일괄 교정하는 프로그램입니다.\n"
+        "\n"
+        "이 프로그램은 LGPL-2.1 라이선스 하에 배포되며, 자유롭게 사용 및 수정할 수 있습니다."
+    )
+
+    messagebox.showinfo(info_title, info_message)
+
+
+def on_github_click():
+    webbrowser.open("https://github.com/itmir913/GPTextVerifier/releases")
+
+
+def on_hash_click(parent):
+    sha_256 = sha_256_hash()
+
+    hash_title = "프로그램 정보"
+    hash_message = (
+        f"SHA-256 Hash: {sha_256}"
+    )
+
+    parent.clipboard_clear()  # 기존 클립보드 내용 삭제
+    parent.clipboard_append(sha_256)  # 새로운 텍스트 추가
+
+    messagebox.showinfo(hash_title, hash_message)
 
 
 if __name__ == "__main__":
